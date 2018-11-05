1dcb7c8850a426eda78215e65bf549ae0d2742f0
CoderHF <54666674@qq.com>
2018-11-05 13:53:26 +0800

    Initial Commit

---
+++ HFBugMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFBugMonitor
+//
+//  Created by CoderHF on 2018/11/5.
+//  Copyright © 2018年 CoderHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
